e55283f715bca45a1742b5dc3dab0d2e8c94b754
George Forman <ghforman@amazon.com>
2020-06-02 13:46:58 -0700

    more work

--- src/main/java/org/djf/mlpaint/MLPaint.java
+++ src/main/java/org/djf/mlpaint/MLPaint.java
@@ -5,8 +5,12 @@ import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
@@ -18,18 +22,41 @@ import javax.swing.SwingUtilities;
 
 import org.djf.util.SwingApp;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
 
 /** Magic Label Paint ~ ML Paint
  * 
  */
 public class MLPaint extends SwingApp {
 	
-	/** current directory */
-	Path directory = Paths.get(".");
+	private static final int UNLABELED = 0;
+	
+	public static void main(String[] args) {
+		//MAYDO: process startup arguments on the command line
+		SwingUtilities.invokeLater(() -> new MLPaint());
+	}
+
+	
+	/** current image file */
+	private Path masterImageFile;
+	
+	/** current image */
+	private BufferedImage masterImage;
+
+	/** labels: 0=UNLABELED, 1=mangrove, 2=not mangrove, ... */
+	private BufferedImage labels;
+	
+	
+	/** layers:  filename & image. */
+	private LinkedHashMap<String, BufferedImage> layers;
+
 	
 	
 	public MLPaint() {
 		super();
+		restoreDirectory(MLPaint.class);
 		setTitle("ML Paint, version 2020.06.02b");// update version number periodically
 		setJMenuBar(makeMenus());
 		setContentPane(makeContentPane());
@@ -50,11 +77,12 @@ public class MLPaint extends SwingApp {
 		file.add(newMenuItem("Save labels...", this::saveLabels));
 		file.add(newMenuItem("Exit", this::exit));
 		
-		
 		JMenu view = new JMenu("View");
 		
 		JMenu label = new JMenu("Label");
-		
+		file.add(newMenuItem("Label positive", this::label));
+		file.add(newMenuItem("Label negative", this::label));
+		file.add(newMenuItem("Delete labeled area", this::label));
 		
 		JMenuBar rr = new JMenuBar();
 		rr.add(file);
@@ -63,47 +91,66 @@ public class MLPaint extends SwingApp {
 		return rr;
 	}
 	
-	protected void openImage(ActionEvent ev) throws IOException {
+	protected void openImage(String command, ActionEvent ev) throws IOException {
 		JFileChooser jfc = new JFileChooser();
-		jfc.setDialogTitle("Open image...");
+		jfc.setDialogTitle("Open images...");
 		jfc.setCurrentDirectory(directory.toFile());
+		jfc.setMultiSelectionEnabled(true);
 		int rr = jfc.showOpenDialog(this);
 		if (rr != JFileChooser.APPROVE_OPTION) {
 			return;
 		}
 		directory = jfc.getCurrentDirectory().toPath();
-		//MAYDO: remember it for future runs
-		File f = jfc.getSelectedFile();
-		BufferedImage img = null;
-		img = ImageIO.read(new File("strawberry.jpg"));
+		storeDirectory(MLPaint.class);// remember it for future runs of the program
+		
+		layers.clear();
+		labels = null;
+		masterImage = null;
+		masterImageFile = null;
+		
+		long t = System.currentTimeMillis();
+		File[] files = jfc.getSelectedFiles();
+		for (File file: files) {
+			BufferedImage img = ImageIO.read(file);
+			t = reportTime(t, "loaded %s", file);
+			layers.put(file.getName(), img);
+			if (file.toString().contains("_RGB")) {
+				masterImage = img;
+				masterImageFile = file.toPath();
+			} else if (file.toString().contains("_labels")) {
+				labels = img;
+			}
+		}
+
+		// if no previously existing labels loaded, create an unlabeled layer
+		if (labels == null) {
+			labels = new BufferedImage(masterImage.getWidth(), masterImage.getHeight(), 
+					BufferedImage.TYPE_BYTE_GRAY);
+			//MAYDO: save RAM   BufferedImage.TYPE_BYTE_BINARY, new ColorModel(with 4 or 16 colors));
+		}
 	}
 
-	protected void saveLabels(ActionEvent ev) {
+	protected void saveLabels(String command, ActionEvent ev) {
+		//TODO  figure out exactly how to output for downstream consumption
 	}
 	
-	protected void exit(ActionEvent ev) {
+	protected void exit(String command, ActionEvent ev) {
 		//TODO: if latest changes not saved
 		// JOptionDialog "Do you want to save your labels first?
 		// save or just quit
 	}
 	
-	protected void open(ActionEvent ev) {
+	protected void label(String command, ActionEvent ev) {
 	}
 	
-	protected void open(ActionEvent ev) {
+	protected void open(String command, ActionEvent ev) {
 	}
 	
-	protected void open(ActionEvent ev) {
+	protected void open(String command, ActionEvent ev) {
 	}
 	
-	protected void open(ActionEvent ev) {
+	protected void open(String command, ActionEvent ev) {
 	}
 	
-	protected void open(ActionEvent ev) {
-	}
 	
-	public static void main(String[] args) {
-		//MAYDO: process startup arguments
-		SwingUtilities.invokeLater(() -> new MLPaint());
-	}
 }
